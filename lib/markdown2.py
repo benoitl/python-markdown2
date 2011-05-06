40f225967eaf29d025050867b78c942907a3c9db
benoitl <benoit.lavigne@windriver.com>
2011-05-06 06:39:08 -0700

    Allow html5 embed tag

--- lib/markdown2.py
+++ lib/markdown2.py
@@ -468,7 +468,7 @@ class Markdown(object):
 
     # I broke out the html5 tags here and add them to _block_tags_a and
     # _block_tags_b.  This way html5 tags are easy to keep track of.
-    _html5tags = '|article|aside|header|hgroup|footer|nav|section|figure|figcaption'
+    _html5tags = '|article|aside|header|hgroup|footer|nav|section|figure|figcaption|embed'
     
     _block_tags_a = 'p|div|h[1-6]|blockquote|pre|table|dl|ol|ul|script|noscript|form|fieldset|iframe|math|ins|del'
     _block_tags_a += _html5tags
